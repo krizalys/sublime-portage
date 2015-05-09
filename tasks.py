757536673c302092e4c3e0714263dcd62918ed57
Christophe Vidal <kriss@krizalys.com>
2015-05-09 11:26:18 +0700

    Refactored/commented tasks.py

--- tasks.py
+++ tasks.py
@@ -4,13 +4,22 @@
 #
 
 #
-# IMPORTANT: This file requires Python 3.4 or newer to be processed.
+# IMPORTANT: This script requires Python 3.4 or newer.
 #
 
 from invoke import task
 import plistlib
 import yaml
 
+#
+# The build task converts all YAML files from the src directory into tmLanguage
+# files into the root directory.
+#
+# NOTE: Although this does not matter from SublimeText, Python does not
+# guarantee that the order of the XML elements in the tmLanguage files will be
+# the same upon each generation. This may affect the list of changed files while
+# committing to the Git repository and should be handled wisely.
+#
 @task
 def build():
     files = {
@@ -33,12 +42,9 @@ def build():
     }
 
     for dest, src in files.items():
-        print(src)
         src    = open("src/" + src)
         dest   = open(dest, "wb")
         syntax = yaml.safe_load(src)
         plistlib.dump(syntax, dest, sort_keys = False)
         dest.close()
         src.close()
-
-    return
